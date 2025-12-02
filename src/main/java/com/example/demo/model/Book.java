e2c6a462e79eb1bdad4b38723bf2b32c019a6ec6
David Cretzu <david.cretu@stud.ubbcluj.ro>
2025-12-02 21:37:57 +0200

    initial-commit

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity /// jba knows its an entity
+@Data /// adds set and get and constructor
+public class Book {
+    @Id /// primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id") /// hybernate
+    private int id;
+}
