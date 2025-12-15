84263a83177fc40d9b8d887dfcc1ba4bb9b7d5bf
Matei Birlea <mateibirlea1@gmail.com>
2025-12-15 20:49:16 +0200

    spring-arhitecture

---
+++ src/main/java/com/example/demo/dto/BookDto.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+
+    private String title;
+
+    private String author;
+
+    private LocalDate publishedDate;
+
+    private int number;
+
+    private int numberOfPages;
+}
